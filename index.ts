a01fc98b22c9b2f67138072e5c35b3b7e7c7e0d3
josh <jpollock412@gmail.com>
2024-04-17 17:26:02 -0400

    set port

--- index.ts
+++ index.ts
@@ -1,5 +1,8 @@
 import { Bot } from "@skyware/bot";
-Bun.serve({
+const PORT: number = +(process.env.PORT || 8081);
+const NODE_ENV = process.env.NODE_ENV ?? "development";
+const server = Bun.serve({
+    port: PORT,
     fetch: async(req) => {
         if( ! process.env.BSKY_USERNAME ){
             return new Response(JSON.stringify({
@@ -24,15 +27,21 @@ Bun.serve({
             });
         try {
             const posts = await bot.getUserLikes('did:plc:payluere6eb3f6j5nbmo2cwy');
-            console.log(Object.keys(posts));
-            let data = posts.posts.map((post) => {
-                return {
-                    text: post.text,
-                    author: post.author,
-                    uri: post.uri,
 
-                };
+            let data = posts.posts.map((post) => {
+                const author = post.author;
+                return Object.assign({},{
+                    text: post.text,
+                    author: {
+                        did: author.did,
+                        handle: author.handle,
+                        displayName: author.displayName,
+                    },
+                    uri: post.uri
+
+                });
             });
+            //@ts-ignore
             return new Response(JSON.stringify(data));
 
         } catch (error) {
@@ -44,3 +53,4 @@ Bun.serve({
         }
     },
 });
+console.log(`[${NODE_ENV}] Serving http://localhost:${server.port}`);
